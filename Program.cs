d9a03025f2b2fabc4906b970d68e5e1533b47e0c
gnx9s <gabriel.rodriguesm0rg@outlook.com>
2023-04-30 21:53:10 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("-- Rolagem de dados --");
+
+Console.WriteLine("Digite quantas faces tem o dado..... ");
+int faces = Convert.ToInt32(Console.ReadLine());
+
+int rolagem = RandomNumberGenerator.GetInt32(1, faces);
+
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine($"A rolagem do dado resultou em {rolagem}");
+Console.ResetColor();
+Console.WriteLine("");
